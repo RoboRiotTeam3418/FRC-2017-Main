c31e1702ed63eb52e7fcc2ecc11b438d1f8b15fd
RoboRiotDev4 <RoboRiotDev4@gmail.com>
2017-02-07 18:16:48 -0600

    commit
    Intake - Ethan Kapelka

--- src/com/team3418/frc2017/Constants.java
+++ src/com/team3418/frc2017/Constants.java
@@ -1,5 +1,9 @@
 package com.team3418.frc2017;
 
 public class Constants {
-
+	public static double kConstantVariableExample = 10.0;
+	
+	//TALONS
+	
+	//SOLENOIDS
 }
--- src/com/team3418/frc2017/subsystems/Intake.java
+++ src/com/team3418/frc2017/subsystems/Intake.java
@@ -1,5 +1,75 @@
 package com.team3418.frc2017.subsystems;
 
-public class Intake {
+import com.team3418.frc2017.Constants;
+import edu.wpi.first.wpilibj.Talon;
 
+public class Intake extends Subsystem
+{
+	static Intake mInstance = new Intake();
+
+    public static Intake getInstance() {
+        return mInstance;
+    }
+    
+	private Talon mIntakeSpinner = new Talon(Constants.kIntakeSpinnerID); //CHANGE TALON ID
+    
+	Intake() {
+		//initialize hardware settings
+		System.out.println("Intake Initialized");
+	}
+    
+    public enum IntakeRollerState {
+    	ROLLER_IN,
+    	ROLLER_OUT,
+    	ROLLER_STOP
+    }
+	
+	private IntakeRollerState mIntakeRollerState;
+
+	public IntakeRollerState getIntakeRollerState() {
+		return mIntakeRollerState;
+	}
+	
+	@Override
+	public void updateSubsystemState()
+	{
+		switch(mIntakeRollerState) {
+		case ROLLER_IN:
+			setRollerSpeed(Constants.kRollerIntakeSpeed); //CHANGE INTAKE CONSTANT
+			break;
+		case ROLLER_OUT:
+			setRollerSpeed(Constants.kRollerReverseSpeed); //CHANGE REVERSE INTAKE CONSTANT
+			break;
+		case ROLLER_STOP:
+			setRollerSpeed(0);
+			break;
+		default:
+			mIntakeRollerState = IntakeRollerState.ROLLER_STOP;
+			break;
+		}
+	}	
+	
+	public void rollerIn(){
+		mIntakeRollerState = IntakeRollerState.ROLLER_IN;
+	}
+	
+	public void rollerOut(){
+		mIntakeRollerState = IntakeRollerState.ROLLER_OUT;
+	}
+	
+	public void stopRoller(){
+		mIntakeRollerState = IntakeRollerState.ROLLER_STOP;
+	}
+	
+
+	private void setRollerSpeed(double speed) {
+		mIntakeSpinner.set(speed);
+	}
+
+	@Override
+	public void outputToSmartDashboard() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
