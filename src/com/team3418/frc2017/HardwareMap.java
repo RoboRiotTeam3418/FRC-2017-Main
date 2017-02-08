5a4ef33e49f5a86cfd0a796288e2da2fed0726b5
nawage <nlwnawage@gmail.com>
2017-02-07 18:09:07 -0600

    main classes created

---
+++ src/com/team3418/frc2017/HardwareMap.java
@@ -0,0 +1,5 @@
+package com.team3418.frc2017;
+
+public class HardwareMap {
+
+}
---
+++ src/com/team3418/frc2017/subsystems/Subsystem.java
@@ -0,0 +1,9 @@
+package com.team3418.frc2017.subsystems;
+
+abstract public class Subsystem {
+	
+	abstract void updateSubsystem();
+	
+	abstract void outputToSmartDashboard();
+
+}
