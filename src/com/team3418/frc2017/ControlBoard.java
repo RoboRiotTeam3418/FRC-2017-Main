a37ac688ff9a8ff84cf51ed75fb231becddd802f
nawage <nlwnawage@gmail.com>
2017-02-09 17:52:41 -0600

    commit

--- src/com/team3418/frc2017/ControlBoard.java
+++ src/com/team3418/frc2017/ControlBoard.java
@@ -12,12 +12,49 @@ public class ControlBoard {
     
     //Create Joystick Object
     private final Joystick mDriverStick;
+    private final Joystick mSecondaryDriverStick;
     
     //Initialize Joystick Object
     ControlBoard() {
     	mDriverStick = new Joystick(0);
+    	mSecondaryDriverStick = new Joystick(1);
     }
     
     //DRIVER CONTROLS
     
+    public double getDriverLeftX(){
+    	return mDriverStick.getRawAxis(0);
+    }
+    
+    public double getDriverLeftY(){
+    	return mDriverStick.getRawAxis(1);
+    }
+    
+    public double getDriverRightX(){
+    	return mDriverStick.getRawAxis(4);
+    }
+    
+    public double getDriverRightY(){
+    	return mDriverStick.getRawAxis(5);
+    }
+    
+    public boolean getDriverHighGearButton(){
+    	return mDriverStick.getRawButton(5);
+    }
+    
+    public boolean getDriverLowGearButton(){
+    	return mDriverStick.getRawButton(6);
+    }
+    
+    public double getClimberAxis(){
+    	return mDriverStick.getRawAxis(2) + -mDriverStick.getRawAxis(3);
+    }
+    
+    public boolean getSecondaryIntakeButton(){
+    	return mSecondaryDriverStick.getRawButton(1);
+    }
+    
+    public boolean getSecondaryShootButton(){
+    	return mSecondaryDriverStick.getRawButton(4);
+    }
 }
